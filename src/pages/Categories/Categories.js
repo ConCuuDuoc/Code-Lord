24b7addd7dde6bb1769ad8a8a198c4ebe118fc5e
yongieghe <21521250@gmail.com>
2023-11-03 22:23:33 +0700

    Them Categories, chua add hieu ung

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -7,62 +7,128 @@ import BE from "./images/back-end-icon.svg";
 import ai from "./images/ai-ml-icon.svg";
 import data from "./images/data-science-icon.svg";
 import networking from "./images/networking-icon.svg";
-import left from "./images/arrow_left_iconsvg.svg";
+import left from "./images/left.svg";
 import right from "./images/right.svg"
+import polygon from "./images/Polygon 8.png"
+import meta from "./images/meta.svg"
 
-
-const Category = ({ className, image, label, skills }) => (
-    <div className={`category-container ${className}`}>
-      <img className="icon" alt="" src={image} />
-      <b>{label}</b>
-      {skills && <span className="skills-youll-gain">{`Skills you'll gain: ${skills}`}</span>}
-      <div className="button-base">
-        <b className="text1">Join</b>
-      </div>
-    </div>
-  );
-  
-
-  const CategoryItem = ({ image, label, title }) => {
-    return (
-      <div className={`category-item ${title}`}>
-        <img className="icon" alt="" src={image} />
-        <div className={`group-${title}-banner-child`} />
-        <div className={`group-${title}-banner-item`} />
-        <b className={title}>{label}</b>
-      </div>
-    );
-  };
-  
 const Categories = () => {
   return (
     <div className="dashboard-categories">
       <div className="all-courses">Career Paths</div>
       <div className="all-courses1">{`Back - End `}</div>
       <div className="group-transition-banner">
-        <CategoryItem className="group-cyber-sec-banner" image={Cyber} label="Cyber Security" />
-        <CategoryItem className="group-frontend-banner" image={FE} label="Front-end" />
-        <CategoryItem className="group-backend-banner" image={BE} label="Back-end" />
-        <CategoryItem className="group-aiml-banner" image={ai} label="AI & ML" />
-        <CategoryItem className="group-datascience-banner" image={data} label="Data Science" />
-        <CategoryItem className="group-networking-banner" image={networking} label="Networking" />
-        <img className="arrow-left-icon-1-4" alt="" src={left} />
-        <img className="arrow-left-icon-1-3" alt="" src={right} />
+        <img className="icon" alt="" src="/9406224-26669-4@2x.png" />
+        <div className="group-cyber-sec-banner">
+          <div className="group-cyber-sec-banner-child" />
+          <div className="group-cyber-sec-banner-item" />
+          <img className="icon-lock" alt="" src={Cyber} />
+          <b className="cyber-security">Cyber Security</b>
+        </div>
+        <div className="group-frontend-banner">
+          <div className="group-frontend-banner-child" />
+          <b className="front-end">Front-end</b>
+          <img
+            className="group-frontend-banner-item"
+            alt=""
+            src={FE}
+          />
+        </div>
+        <div className="group-backend-banner">
+          <div className="group-backend-banner-child" />
+          <div className="group-backend-banner-item" />
+          <b className="back-end">Back-end</b>
+          <img className="layers-icon" alt="" src={BE} />
+        </div>
+        <div className="group-aiml-banner">
+          <div className="group-cyber-sec-banner-child" />
+          <div className="group-aiml-banner-item" />
+          <b className="ai-ml">{`AI & ML`}</b>
+          <img
+            className="icon-people-rounded"
+            alt=""
+            src={ai}
+          />
+        </div>
+        <div className="group-datascience-banner">
+          <div className="group-cyber-sec-banner-child" />
+          <b className="data-science">Data Science</b>
+          <div className="group-datascience-banner-item" />
+          <img className="icon-archive" alt="" src={data} />
+        </div>
+        <div className="group-networking-banner">
+          <div className="group-frontend-banner-child" />
+          <div className="group-datascience-banner-item" />
+          <b className="networking">{`Networking  `}</b>
+          <img
+            className="icon-network-alt"
+            alt=""
+            src={networking}
+          />
+        </div>
+        <img
+          className="arrow-left-icon-1-4"
+          alt=""
+          src={left}
+        />
+        <img
+          className="arrow-left-icon-1-3"
+          alt=""
+          src={right}
+        />
       </div>
-      <Category
-        className="group-dev-backend"
-        image="/polygon-12.svg"
-        label="Developing Back-End Apps with Node.js and Express"
-        skills="Computer Programming, Web Development"
-      />
-      <Category 
-      className="group-introduce-front-end" 
-      image="/polygon-9.svg" 
-      label="Introduction to Back - End Development" 
-      skills="Web Development" />
+      <div className="group-dev-backend">
+        <div className="group-dev-backend-child" />
+        <b className="developing-back-end-apps">
+          Developing Back-End Apps with Node.js and Express
+        </b>
+        <div className="group-dev-backend-item" />
+        <div className="x-lesson">38x Lesson</div>
+        <img className="group-dev-backend-inner" alt="" src={polygon} />
+        <div className="line-div" />
+        <div className="text">
+          <span className="skills-youll-gain">{`Skills you'll gain: `}</span>
+          <span>{`Computer Programming, Web Development `}</span>
+        </div>
+        <div className="button-base">
+          <b className="text1">Join</b>
+        </div>
+        <div className="rectangle-div" />
+        <div className="back-end1">Back-end</div>
+        <img className="rectangle-icon" alt="" src={meta} />
+      </div>
+      <div className="group-introduce-front-end">
+        <div className="group-introduce-front-end-child" />
+        <b className="introduction-to-back">
+          Introduction to Back - End Development
+        </b>
+        <div className="group-introduce-front-end-item" />
+        <div className="x-lesson1">25x Lesson</div>
+        <img
+          className="group-introduce-front-end-inner"
+          alt=""
+          src={polygon}
+        />
+        <div className="group-introduce-front-end-child1" />
+        <div className="group-introduce-front-end-child2" />
+        <div className="front-end1">Back-end</div>
+        <img
+          className="group-introduce-front-end-child3"
+          alt=""
+          src={meta}
+        />
+        <div className="text">
+          <span className="skills-youll-gain">Skills you'll gain:</span>
+          <span> Web Development</span>
+        </div>
+        <div className="button-base1">
+          <b className="text1">Join</b>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Categories;
+
 
