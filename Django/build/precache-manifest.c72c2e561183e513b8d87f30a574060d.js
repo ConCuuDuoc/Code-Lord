1537b5d5bd11a898ed6962cbf68b701baa34a95e
kietngo255 <21521034@gm.uit.edu.vn>
2023-11-22 03:57:43 +0700

    A little bit testing

---
+++ Django/build/precache-manifest.c72c2e561183e513b8d87f30a574060d.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6296807a23447c29b27dd1951ac224a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee29eee639fd79359831",
+    "url": "/static/css/2.6f2db2d8.chunk.css"
+  },
+  {
+    "revision": "4c2d44c53e38561855f9",
+    "url": "/static/css/main.aea2b328.chunk.css"
+  },
+  {
+    "revision": "ee29eee639fd79359831",
+    "url": "/static/js/2.2784afa7.chunk.js"
+  },
+  {
+    "revision": "aea3a49c1b1f6fbd8c4bcd604c27fb2b",
+    "url": "/static/js/2.2784afa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6f257e590093ef6523d",
+    "url": "/static/js/3.2fc84299.chunk.js"
+  },
+  {
+    "revision": "4c2d44c53e38561855f9",
+    "url": "/static/js/main.6b150bdd.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6b150bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261361da0dfcfad55b16",
+    "url": "/static/js/runtime-main.e6553b5c.js"
+  },
+  {
+    "revision": "ced0b7afebfb0cd756567cd572346df9",
+    "url": "/static/media/213.ced0b7af.jpg"
+  },
+  {
+    "revision": "92fd792c6aab05a43fea342468e44c1c",
+    "url": "/static/media/3 User.92fd792c.svg"
+  },
+  {
+    "revision": "35fe2b41a5550b83a01655504573df37",
+    "url": "/static/media/Allura Avatar.35fe2b41.svg"
+  },
+  {
+    "revision": "d905afaa06a045ce68ee39e57dce9e00",
+    "url": "/static/media/Cart.d905afaa.svg"
+  },
+  {
+    "revision": "01806f0c8bb0ef9f535babec265d7309",
+    "url": "/static/media/Document.01806f0c.svg"
+  },
+  {
+    "revision": "6855d57459b6f1a131d01ed33e716285",
+    "url": "/static/media/Document.6855d574.svg"
+  },
+  {
+    "revision": "c188fcb9b2204e261079b820ebcea83c",
+    "url": "/static/media/Group 28.c188fcb9.svg"
+  },
+  {
+    "revision": "f1c82ed175a35e4582a4cf05f757fa17",
+    "url": "/static/media/Group 29.f1c82ed1.svg"
+  },
+  {
+    "revision": "872d5233853f3acf199519e5fa11803e",
+    "url": "/static/media/Group.872d5233.svg"
+  },
+  {
+    "revision": "1d8944582598d308e9b642d70722914c",
+    "url": "/static/media/Group1.1d894458.svg"
+  },
+  {
+    "revision": "64a9665c39a741543a7ae007a809967b",
+    "url": "/static/media/Home.64a9665c.svg"
+  },
+  {
+    "revision": "fa5c39cb9f9eb2519c2a2bd84dfe78b2",
+    "url": "/static/media/Image 2.fa5c39cb.svg"
+  },
+  {
+    "revision": "ceeb62ad018418f88cc13b6e29a7fc13",
+    "url": "/static/media/Logout.ceeb62ad.svg"
+  },
+  {
+    "revision": "5e8d15a718143a17f73e7764846fe33b",
+    "url": "/static/media/Notification.5e8d15a7.svg"
+  },
+  {
+    "revision": "a8514a34e2f398df2edc0d10010e9c50",
+    "url": "/static/media/Polygon 1.a8514a34.svg"
+  },
+  {
+    "revision": "be28f964779b5090710b678d0543f983",
+    "url": "/static/media/Setting.be28f964.svg"
+  },
+  {
+    "revision": "f88b81d7234d9db482ebab69ee1ecd6e",
+    "url": "/static/media/ai-ml-icon.f88b81d7.svg"
+  },
+  {
+    "revision": "ba65086fbc186e7ee84c45a42d64c3a5",
+    "url": "/static/media/back-end-icon.ba65086f.svg"
+  },
+  {
+    "revision": "035c9b20dcfd786292244451cf011b2d",
+    "url": "/static/media/background2.035c9b20.svg"
+  },
+  {
+    "revision": "d71e94b63f421170a75fe79d08b26e35",
+    "url": "/static/media/cisco.d71e94b6.svg"
+  },
+  {
+    "revision": "24779f296f16487de223d517eee04899",
+    "url": "/static/media/credit_card.24779f29.svg"
+  },
+  {
+    "revision": "8cab6914cd808c8190225f59ad9a7eff",
+    "url": "/static/media/cyber-security-icon.8cab6914.svg"
+  },
+  {
+    "revision": "36a006b7caa38b715f79ee430cae3e7c",
+    "url": "/static/media/data-science-icon.36a006b7.svg"
+  },
+  {
+    "revision": "98d3e0934fdef71d73ecc6cb11f5702b",
+    "url": "/static/media/facebook.98d3e093.svg"
+  },
+  {
+    "revision": "b8ee2d341ffac25b8008dd20d3760c0f",
+    "url": "/static/media/front-end-icon.b8ee2d34.svg"
+  },
+  {
+    "revision": "2460bc5391d762205f3952e7a0660c75",
+    "url": "/static/media/github.2460bc53.svg"
+  },
+  {
+    "revision": "0a80e4bb5a431deef498e9e4e5b3fbf6",
+    "url": "/static/media/google.0a80e4bb.svg"
+  },
+  {
+    "revision": "f96bce86c9e0ca1fa7302dd727bec9cb",
+    "url": "/static/media/google.f96bce86.svg"
+  },
+  {
+    "revision": "c96df51fb80d2c0621ad1b123bd76ad2",
+    "url": "/static/media/ibm.c96df51f.png"
+  },
+  {
+    "revision": "9cfddb9cd767a851c1f96b756fb63179",
+    "url": "/static/media/icon-play-yellow.9cfddb9c.svg"
+  },
+  {
+    "revision": "9cf0f8780f13710a8c37576ddd8d7a56",
+    "url": "/static/media/img1.9cf0f878.svg"
+  },
+  {
+    "revision": "22afe411e9514c1ff03d34c4af80fe0a",
+    "url": "/static/media/img2.22afe411.svg"
+  },
+  {
+    "revision": "6d52b77c33a6a923105844614e2a3f0c",
+    "url": "/static/media/leftimagesbackground.6d52b77c.svg"
+  },
+  {
+    "revision": "90f917f047dc98cf1e5c597054afe006",
+    "url": "/static/media/line-1.90f917f0.svg"
+  },
+  {
+    "revision": "5efb8a44c03fe03b0163fad5388e8d8f",
+    "url": "/static/media/line.5efb8a44.svg"
+  },
+  {
+    "revision": "131d4d5c5b5461e3e676f955a78bd1e5",
+    "url": "/static/media/meta.131d4d5c.svg"
+  },
+  {
+    "revision": "f8f260d562c982ed0666b30e026eafbe",
+    "url": "/static/media/meta.f8f260d5.svg"
+  },
+  {
+    "revision": "1c03613ae28accdf309015e0974db035",
+    "url": "/static/media/middleimagesbackground.1c03613a.svg"
+  },
+  {
+    "revision": "d892f552958d7c01de0b609967f76bc1",
+    "url": "/static/media/networking-icon.d892f552.svg"
+  },
+  {
+    "revision": "e1744fc3ea1c18a955506c6ae8d0ad9b",
+    "url": "/static/media/polygon.e1744fc3.svg"
+  },
+  {
+    "revision": "9e7136060da2f6edfc0bc1466d0242dc",
+    "url": "/static/media/rectangle-5.9e713606.svg"
+  },
+  {
+    "revision": "a960400a44c0d86b264d9300335ba948",
+    "url": "/static/media/rightimagesbackground.a960400a.svg"
+  },
+  {
+    "revision": "541f3ca3b2a97ec54d9c32461c11932b",
+    "url": "/static/media/star.541f3ca3.svg"
+  },
+  {
+    "revision": "c12cf6818e30574bc34792d686ffd9f1",
+    "url": "/static/media/star2.c12cf681.svg"
+  },
+  {
+    "revision": "20f51f97fa1fd2c251eca794c1bd1ea2",
+    "url": "/static/media/target.20f51f97.svg"
+  },
+  {
+    "revision": "57634e0dac04e6816e33b25718a2415e",
+    "url": "/static/media/woman-FAQ.57634e0d.svg"
+  },
+  {
+    "revision": "3eda771a676fb36d6ed4b2ba2efa6e53",
+    "url": "/static/media/woman.3eda771a.svg"
+  },
+  {
+    "revision": "d7fee16b2f5d147607dba7a9fc7426fc",
+    "url": "/static/media/work.d7fee16b.svg"
+  }
+]);
